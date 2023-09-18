9ad4c13a7a3ec97e8ba1faf08ffee359b2605a79
Beomsu <gobeumsu@gmail.com>
2023-09-18 19:28:20 +0900

    feat: 스프링 부트와 자바 설정 업데이트
    - 스프링 부트 버전을 2.7.15로 업데이트했습니다.
    - 자바 소스 호환성을 17로 설정했습니다.
    - Lombok과 Spring Boot Starter Web 의존성을 추가했습니다.

---
+++ src/test/java/com/kakaotech/team14backend/Team14BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaotech.team14backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team14BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
