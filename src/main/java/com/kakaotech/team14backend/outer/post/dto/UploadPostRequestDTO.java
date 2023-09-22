75e45e2510063167218f3d68564f6350d78520e3
Beomsu <gobeumsu@gmail.com>
2023-09-23 00:19:29 +0900

    feat: PostController 및 UploadPostRequestDTO 구현
    PostController에서는 게시글 업로드를 담당하는 API 엔드포인트를 구현하였습니다.
    이를 위해 UploadPostRequestDTO를 통해 필요한 요청 데이터를 받고 있습니다.
    이러한 구현은 클라이언트로부터 게시글 관련 정보를 안전하고 효율적으로 처리할 수 있도록 해줍니다.
    
    이슈번호: #7
    
    Co-Authored-By: Sonny <91835827+hwangdaesun@users.noreply.github.com>

---
+++ src/main/java/com/kakaotech/team14backend/outer/post/dto/UploadPostRequestDTO.java
@@ -0,0 +1,14 @@
+package com.kakaotech.team14backend.outer.post.dto;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class UploadPostRequestDTO {
+
+  private List<String> hashTags;
+  private String nickname;
+  private String imageName;
+  private String university;
+
+}
