ccfe9c3402a13eed04f40d541ef55f53a57b047c
Beomsu <gobeumsu@gmail.com>
2023-09-23 00:13:13 +0900

    feat: Post, Image, Member 엔티티와 레퍼지터리 구현
    Post, Image, Member 엔티티와 레퍼지터리를 구현하고, 각 엔티티간의 관계를 설정하였습니다.
    
    이슈번호: #7
    
    Co-Authored-By: Sonny <91835827+hwangdaesun@users.noreply.github.com>

---
+++ src/main/java/com/kakaotech/team14backend/inner/member/model/Member.java
@@ -0,0 +1,49 @@
+package com.kakaotech.team14backend.inner.member.model;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import com.kakaotech.team14backend.inner.post.model.Post;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access=PROTECTED)
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long memberId;
+
+  @OneToMany(mappedBy = "member")
+  private List<Post> posts = new ArrayList<>();
+
+  @Column(nullable = false, length = 50)
+  private String userName; // 유저 이름
+
+  @Column(nullable = false, length = 50)
+  private String kakaoId; // 카카오 아이디
+
+  @Column(nullable = false, length = 50)
+  private String instaId; // 인스타그램 아이디
+
+  @Column(nullable = false)
+  private Long totalLike = 0L; // 보유 좋아요 수
+
+  @Column(nullable = false)
+  private LocalDateTime createdAt; // 생성일
+
+  @Column(nullable = false)
+  private LocalDateTime updatedAt; // 수정일
+
+  @Column(nullable = false,length = 20)
+  private String userStatus; // 제재, 탈퇴, 정상 등등
+
+}
