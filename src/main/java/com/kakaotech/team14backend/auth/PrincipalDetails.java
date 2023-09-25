c198260c38793f6db08bbccfe6864dcdd99944a2
yuseonkim <yusun715@icloud.com>
2023-09-25 18:53:45 +0900

    feat : PrincipalDetails 클래스 작성 및 AuthenticationSuccessHandler
        로그인 및 검증 후 사용자정보를 담기위한 PrincipalDetails 클래스를 작성하였습니다.
        로그인 완료 후 실행할 로직을 작성한 AuthenticationSuccessHandler 클래스를 작성하였습니다.

---
+++ src/main/java/com/kakaotech/team14backend/auth/PrincipalDetails.java
@@ -0,0 +1,82 @@
+package com.kakaotech.team14backend.auth;
+
+
+import com.kakaotech.team14backend.inner.member.model.Member;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@Data
+public class PrincipalDetails implements UserDetails,OAuth2User {
+
+    private Member member;
+    private Map<String,Object> attributes;
+    public PrincipalDetails(Member member,Map<String,Object> attributes){
+        this.member = member;
+        this.attributes = attributes;
+    }
+    public PrincipalDetails(Member member){
+        this.member = member;
+    }
+
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return member.getRole();
+            }
+        });
+
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return member.getUsername();
+    }
+
+
+}
