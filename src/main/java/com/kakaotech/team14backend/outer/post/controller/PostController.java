fb2ba4b4fbf3b086ff2b799e61a72a05f2068684
Beomsu <gobeumsu@gmail.com>
2023-09-29 00:34:43 +0900

    feat: 홈 피드 조회 기능 구현
    - `PostService` 클래스에서 홈 피드를 조회하는 로직을 구현했습니다.
    - `FindPostListUsecase`를 이용하여 Post 게시물을 가져옵니다.
    - 현재 로직에 따르면, Post 게시물을 최신 순으로 10개씩 반환합니다.
    - 반환된 게시물 리스트는 `GetPostResponseDTO`로 변환됩니다.
    
    이슈: #18

--- src/main/java/com/kakaotech/team14backend/outer/post/controller/PostController.java
+++ src/main/java/com/kakaotech/team14backend/outer/post/controller/PostController.java
@@ -8,12 +8,14 @@ import com.kakaotech.team14backend.outer.post.dto.UploadPostDTO;
 import com.kakaotech.team14backend.outer.post.dto.UploadPostRequestDTO;
 import com.kakaotech.team14backend.outer.post.service.PostService;
 import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,6 +26,16 @@ import org.springframework.web.multipart.MultipartFile;
 public class PostController {
 
   private final PostService postService;
+
+  @GetMapping("/post")
+  public ApiResponse<ApiResponse.CustomBody<List<GetPostResponseDTO>>> getPosts(@RequestParam(defaultValue = "10") int limit ) {
+
+    // todo value 값에 따라 게시물 반환 갯수 조절
+
+    List<GetPostResponseDTO> getPostListResponseDTO = postService.getPostList();
+    return ApiResponseGenerator.success(getPostListResponseDTO,HttpStatus.CREATED);
+  }
+
 
   @PostMapping("/post")
   public ApiResponse<ApiResponse.CustomBody<Void>> uploadPost(@RequestPart MultipartFile image,
--- src/main/java/com/kakaotech/team14backend/outer/post/service/PostService.java
+++ src/main/java/com/kakaotech/team14backend/outer/post/service/PostService.java
@@ -5,6 +5,7 @@ import com.kakaotech.team14backend.inner.image.usecase.CreateImageUsecase;
 import com.kakaotech.team14backend.inner.member.model.Member;
 import com.kakaotech.team14backend.inner.member.usecase.FindMemberUsecase;
 import com.kakaotech.team14backend.inner.post.model.Post;
+import com.kakaotech.team14backend.inner.post.usecase.FindPostListUsecase;
 import com.kakaotech.team14backend.inner.post.usecase.FindPostUsecase;
 import com.kakaotech.team14backend.outer.post.dto.CreatePostDTO;
 import com.kakaotech.team14backend.outer.post.dto.GetPostDTO;
@@ -15,6 +16,7 @@ import com.kakaotech.team14backend.inner.post.usecase.CreatePostUsecase;
 import java.io.IOException;
 
 import com.kakaotech.team14backend.outer.post.mapper.PostMapper;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +31,7 @@ public class PostService {
   private final CreatePostUsecase createPostUsecase;
   private final FindMemberUsecase findMemberUsecase;
   private final FindPostUsecase findPostUsecase;
-
+  private final FindPostListUsecase findPostListUsecase;
   @Transactional
   public void uploadPost(UploadPostDTO uploadPostDTO) throws IOException {
     Member savedMember = findMemberUsecase.execute(uploadPostDTO.memberId());
@@ -42,6 +44,12 @@ public class PostService {
     Post post = findPostUsecase.execute(getPostDTO);
     GetPostResponseDTO getPostResponseDTO = PostMapper.from(post);
     return getPostResponseDTO;
+  }
+
+  public List<GetPostResponseDTO> getPostList(){
+    List<Post> postList = findPostListUsecase.excute();
+    List<GetPostResponseDTO> editedPostList =  PostMapper.from(postList);
+    return editedPostList;
   }
 
 }
