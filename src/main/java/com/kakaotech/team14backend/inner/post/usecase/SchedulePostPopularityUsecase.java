cb67d8be0fc136281ca3e6fc1be643fa7b09b0a9
hwangdaesun <hds650588@gmail.com>
2023-10-04 19:58:58 +0900

    fix : @Scheduled와 @Transactional 문제로 인한 클래스 분리

--- src/main/java/com/kakaotech/team14backend/inner/post/usecase/SchedulePostPopularityUsecase.java
+++ src/main/java/com/kakaotech/team14backend/inner/post/usecase/SchedulePostPopularityUsecase.java
@@ -1,26 +1,19 @@
 package com.kakaotech.team14backend.inner.post.usecase;
 
-import com.kakaotech.team14backend.inner.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
 
 
 @Component
 @RequiredArgsConstructor
 public class SchedulePostPopularityUsecase {
 
-  private final PostRepository postRepository;
+  private final UpdatePostPopularityUsecase updatePostPopularityUsecase;
 
-  @Transactional
   @Scheduled(initialDelayString = "${schedules.initialDelay}",fixedDelayString = "${schedules.fixedDelay}")
   public void execute() {
-    postRepository.findAll().stream().forEach(post -> {
-      Long likeCount = post.getPostLike().getLikeCount();
-      long postAge = post.measurePostAge();
-      post.updatePopularity(likeCount.longValue(), postAge);
-    });
+    updatePostPopularityUsecase.execute();
   }
+
 }
