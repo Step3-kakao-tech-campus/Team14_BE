f65df52a5b2783bc9aee81db82bf721162286773
Beomsu <gobeumsu@gmail.com>
2023-10-06 11:48:31 +0900

    feat : Post의 좋아요 Count 기능 추가

--- src/main/java/com/kakaotech/team14backend/outer/post/dto/GetPostLikeHistoryDTO.java
+++
@@ -1,8 +0,0 @@
-package com.kakaotech.team14backend.outer.post.dto;
-
-import com.kakaotech.team14backend.inner.member.model.Member;
-import com.kakaotech.team14backend.inner.post.model.Post;
-
-public record GetPostLikeHistoryDTO(Member member, Post post) {
-
-}
