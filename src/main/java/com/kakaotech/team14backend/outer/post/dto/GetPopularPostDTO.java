49a068ac8c6a9ac109025615450dbfe632a5696d
hwangdaesun <hds650588@gmail.com>
2023-10-07 02:10:15 +0900

    feat : 인기 게시물들 조회하는 controller layer 구현

--- src/main/java/com/kakaotech/team14backend/outer/post/dto/GetPopularPostDTO.java
+++ src/main/java/com/kakaotech/team14backend/outer/post/dto/GetPopularPostDTO.java
@@ -1,28 +1,6 @@
 package com.kakaotech.team14backend.outer.post.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.List;
 
-@Data
-@NoArgsConstructor
-public class GetPopularPostDTO {
-
-  private Long postId;
-  private String imageUri;
-  private String hashTag;
-  private Long likeCount;
-  private Integer postPoint;
-  private Long popularity;
-  private String nickname;
-
-  public GetPopularPostDTO(Long postId, String imageUri, String hashTag, Long likeCount, Integer postPoint, Long popularity, String nickname) {
-    this.postId = postId;
-    this.imageUri = imageUri;
-    this.hashTag = hashTag;
-    this.likeCount = likeCount;
-    this.postPoint = postPoint;
-    this.popularity = popularity;
-    this.nickname = nickname;
-  }
+public record GetPopularPostDTO(Long postId, String imageUri, List<String> hashTags, Long likeCount, Integer boardPoint, String nickname, Integer postLevel) {
 }
