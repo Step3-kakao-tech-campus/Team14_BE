86784003acf6ff3ed078667f31ff22983ea89f29
yuseonkim <yusun715@icloud.com>
2023-10-06 14:53:59 +0900

    refactor : AuthenticationSuccessHandler 클래스 수정
    새로 추가된 기능들을 사용하게 코드를 변경하였습니다.
    
    이슈번호:#51

--- src/main/java/com/kakaotech/team14backend/auth/AuthenticationSuccessHandler.java
+++ src/main/java/com/kakaotech/team14backend/auth/AuthenticationSuccessHandler.java
@@ -6,8 +6,8 @@ import com.kakaotech.team14backend.common.ApiResponse;
 import com.kakaotech.team14backend.common.ApiResponseGenerator;
 import com.kakaotech.team14backend.inner.member.model.Member;
 import com.kakaotech.team14backend.inner.member.repository.MemberRepository;
-import com.kakaotech.team14backend.jwt.JwtProvider;
-import com.kakaotech.team14backend.outer.login.dto.TokenDTO;
+import com.kakaotech.team14backend.jwt.service.TokenService;
+import com.kakaotech.team14backend.jwt.dto.TokenDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -21,6 +21,7 @@ import java.io.IOException;
 @Component
 public class AuthenticationSuccessHandler implements org.springframework.security.web.authentication.AuthenticationSuccessHandler {
 
+  private final TokenService tokenService;
   private final MemberRepository memberRepository;
 
   @Override
@@ -28,13 +29,13 @@ public class AuthenticationSuccessHandler implements org.springframework.securit
     PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
     String kakaoId = Long.toString(principalDetails.getAttribute("id"));
     Member member = memberRepository.findByKakaoId(kakaoId);
-    String accessToken = JwtProvider.create(member);
-    TokenDTO tokenDTO = new TokenDTO();
-    tokenDTO.setAccessToken(accessToken);
+    TokenDTO tokenDTO = tokenService.createOrUpdateToken(response,member);
 //    ApiResponse<?> apiResponse = new ApiResponse<>(new ApiResponse.CustomBody(true,"accessToken:" + jwt,null), HttpStatus.OK);
     ApiResponse<?> apiResponse = ApiResponseGenerator.success(tokenDTO,HttpStatus.OK);
     response.setContentType("application/json");
     response.getWriter().write(new ObjectMapper().writeValueAsString(apiResponse));
   }
+
+
 }
 
