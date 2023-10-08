72151528db3212054442ed7a8b34b1f6c240d06a
yuseonkim <yusun715@icloud.com>
2023-10-08 18:33:08 +0900

    refactor : AuthenticationSuccessHandler 리팩토링
    수정사항 없음..

--- src/main/java/com/kakaotech/team14backend/auth/AuthenticationSuccessHandler.java
+++ src/main/java/com/kakaotech/team14backend/auth/AuthenticationSuccessHandler.java
@@ -4,6 +4,7 @@ package com.kakaotech.team14backend.auth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kakaotech.team14backend.common.ApiResponse;
 import com.kakaotech.team14backend.common.ApiResponseGenerator;
+import com.kakaotech.team14backend.exception.TokenValidationException;
 import com.kakaotech.team14backend.inner.member.model.Member;
 import com.kakaotech.team14backend.inner.member.repository.MemberRepository;
 import com.kakaotech.team14backend.jwt.service.TokenService;
@@ -26,16 +27,17 @@ public class AuthenticationSuccessHandler implements org.springframework.securit
 
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+
     PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
     String kakaoId = Long.toString(principalDetails.getAttribute("id"));
     Member member = memberRepository.findByKakaoId(kakaoId);
-    TokenDTO tokenDTO = tokenService.createOrUpdateToken(response,member);
+    TokenDTO tokenDTO = tokenService.createOrUpdateToken(response, member);
 //    ApiResponse<?> apiResponse = new ApiResponse<>(new ApiResponse.CustomBody(true,"accessToken:" + jwt,null), HttpStatus.OK);
-    ApiResponse<?> apiResponse = ApiResponseGenerator.success(tokenDTO,HttpStatus.OK);
+    ApiResponse<?> apiResponse = ApiResponseGenerator.success(tokenDTO, HttpStatus.OK);
     response.setContentType("application/json");
     response.getWriter().write(new ObjectMapper().writeValueAsString(apiResponse));
+
   }
-
-
 }
+
 
