a62592b141b60304cf118a55402b02146f5136a8
yuseonkim <yusun715@icloud.com>
2023-10-08 18:16:34 +0900

    feat : MessageCode 토큰 관련 에러 추가
    reissue api에 쓰일 리프레시토큰 인증 실패에 대한 MessageCode를 작성하였습니다
    
    이슈번호 : #63

--- src/main/java/com/kakaotech/team14backend/common/MessageCode.java
+++ src/main/java/com/kakaotech/team14backend/common/MessageCode.java
@@ -3,7 +3,15 @@ package com.kakaotech.team14backend.common;
 
 public enum MessageCode {
 
-  NOT_ALLOWED_FILE_EXT("4003","파일 확장명은 pdf,img만 가능합니다.");
+  NOT_ALLOWED_FILE_EXT("4003","파일 확장명은 pdf,img만 가능합니다."),
+  INVALIDATE_REFRESH_TOKEN("401","잘못된 리프레시 토큰입니다."),
+  INVALIDATE_ACCESS_TOKEN("401","잘못된 엑세스 토큰입니다.")
+  ,
+  INCORRECT_REFRESH_TOEKN("401","요청된 리프레시토큰과 불일치합니다")
+,
+  NOT_REGISTER_MEMBER("401","회원정보가 존재하지 않습니다")
+  ,;
+
   private final String code;
   private final String value;
 
