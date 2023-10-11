e76016027f2762148bf7e1ff37f09025d143ee3c
yuseonkim <yusun715@icloud.com>
2023-10-11 13:28:55 +0900

    style : Controller 클래스 줄바꿈

--- src/main/java/com/kakaotech/team14backend/outer/login/controller/LoginController.java
+++ src/main/java/com/kakaotech/team14backend/outer/login/controller/LoginController.java
@@ -1,19 +1,29 @@
 package com.kakaotech.team14backend.outer.login.controller;
 
+import com.kakaotech.team14backend.common.ApiResponseGenerator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
 
   @GetMapping("/api/login")
-  public String loginOAuth2(){
+  public String loginOAuth2() {
     return "redirect:/oauth2/authorization/kakao";
   }
+
   @ResponseBody
   @GetMapping("/api/user/instagram")
-  public String info(){
+  public String info() {
     return "instagram";
+
   }
+
+
 }
