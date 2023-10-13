dd1d9883ef8fe448919f338f83fb50679f7e25b9
hwangdaesun <hds650588@gmail.com>
2023-10-13 15:58:24 +0900

    refactor : createPointUsecase 추가 및 todo 주석 추가
    Co-authored-by: GoBeromsu <gobeumsu@gmail.com>

--- src/main/java/com/kakaotech/team14backend/outer/member/service/MemberService.java
+++ src/main/java/com/kakaotech/team14backend/outer/member/service/MemberService.java
@@ -3,10 +3,19 @@ package com.kakaotech.team14backend.outer.member.service;
 import com.kakaotech.team14backend.inner.member.model.Member;
 import com.kakaotech.team14backend.inner.member.model.Role;
 import com.kakaotech.team14backend.inner.member.model.Status;
+import com.kakaotech.team14backend.inner.point.model.PointPolicy;
+import com.kakaotech.team14backend.inner.point.usecase.CreatePointUsecase;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+
+  private final CreatePointUsecase createPointUsecase;
+
+  //private final CreateMemberUsecase createMemberUsecase;
+
   public static Member createMember(String userName, String kakaoId, String instaId, Role role, Long totalLike, Status userStatus) {
     return Member.builder()
         .userName(userName)
