948d7472e35155d59f91f684d10f1f7241064250
hwangdaesun <hds650588@gmail.com>
2023-10-14 16:33:50 +0900

    refactor : Post 엔티티에 postLevel 컬럼 추가 및 postPoint 컬럼 제거에 따른 SQL 문 수정

--- src/main/resources/db/teardown.sql
+++ src/main/resources/db/teardown.sql
@@ -27,28 +27,27 @@ VALUES (NOW(), 'image_uri1'),
 
 -- Post Table
 INSERT INTO post (created_at, nickname, popularity, published, report_count, university, view_count,
-                  post_point,
                   image_id, member_id, hashtag)
-VALUES (NOW(), 'nickname1', 100, true, 0, 'university1', 1000, 400, 1, 1, '#hashtag1'),
-       (NOW(), 'nickname2', 200, true, 1, 'university2', 2000, 400, 2, 2, '#hashtag2'),
-       (NOW(), 'nickname3', 300, false, 2, 'university3', 3000, 400, 3, 3, '#hashtag3'),
-       (NOW(), 'nickname4', 400, true, 3, 'university4', 4000, 400, 4, 1, '#hashtag4'),
-       (NOW(), 'nickname5', 500, false, 4, 'university5', 5000, 400, 5, 2, '#hashtag5'),
-       (NOW(), 'nickname6', 600, true, 5, 'university6', 6000, 400, 6, 3, '#hashtag6'),
-       (NOW(), 'nickname7', 700, false, 6, 'university7', 7000, 400, 7, 1, '#hashtag7'),
-       (NOW(), 'nickname8', 800, true, 7, 'university8', 8000, 400, 8, 2, '#hashtag8'),
-       (NOW(), 'nickname9', 900, false, 8, 'university9', 9000, 400, 9, 3, '#hashtag9'),
-       (NOW(), 'nickname10', 1000, true, 9, 'university10', 10000, 400, 10, 1, '#hashtag10'),
-       (NOW(), 'nickname11', 100, true, 0, 'university1', 1000, 400, 1, 1, '#hashtag1'),
-       (NOW(), 'nickname12', 200, true, 1, 'university2', 2000, 400, 2, 2, '#hashtag2'),
-       (NOW(), 'nickname13', 300, false, 2, 'university3', 3000, 400, 3, 3, '#hashtag3'),
-       (NOW(), 'nickname14', 400, true, 3, 'university4', 4000, 400, 4, 1, '#hashtag4'),
-       (NOW(), 'nickname15', 500, false, 4, 'university5', 5000, 400, 5, 2, '#hashtag5'),
-       (NOW(), 'nickname16', 600, true, 5, 'university6', 6000, 400, 6, 3, '#hashtag6'),
-       (NOW(), 'nickname17', 700, false, 6, 'university7', 7000, 400, 7, 1, '#hashtag7'),
-       (NOW(), 'nickname18', 800, true, 7, 'university8', 8000, 400, 8, 2, '#hashtag8'),
-       (NOW(), 'nickname19', 900, false, 8, 'university9', 9000, 400, 9, 3, '#hashtag9'),
-       (NOW(), 'nickname20', 1000, true, 9, 'university10', 10000, 400, 10, 1, '#hashtag10');
+VALUES (NOW(), 'nickname1', 100, true, 0, 'university1', 1000, 1, 1, '#hashtag1'),
+       (NOW(), 'nickname2', 200, true, 1, 'university2', 2000, 2, 2, '#hashtag2'),
+       (NOW(), 'nickname3', 300, false, 2, 'university3', 3000, 3, 3, '#hashtag3'),
+       (NOW(), 'nickname4', 400, true, 3, 'university4', 4000, 4, 1, '#hashtag4'),
+       (NOW(), 'nickname5', 500, false, 4, 'university5', 5000, 5, 2, '#hashtag5'),
+       (NOW(), 'nickname6', 600, true, 5, 'university6', 6000, 6, 3, '#hashtag6'),
+       (NOW(), 'nickname7', 700, false, 6, 'university7', 7000, 7, 1, '#hashtag7'),
+       (NOW(), 'nickname8', 800, true, 7, 'university8', 8000, 8, 2, '#hashtag8'),
+       (NOW(), 'nickname9', 900, false, 8, 'university9', 9000, 9, 3, '#hashtag9'),
+       (NOW(), 'nickname10', 1000, true, 9, 'university10', 10000, 10, 1, '#hashtag10'),
+       (NOW(), 'nickname11', 100, true, 0, 'university1', 1000, 1, 1, '#hashtag1'),
+       (NOW(), 'nickname12', 200, true, 1, 'university2', 2000, 2, 2, '#hashtag2'),
+       (NOW(), 'nickname13', 300, false, 2, 'university3', 3000, 3, 3, '#hashtag3'),
+       (NOW(), 'nickname14', 400, true, 3, 'university4', 4000, 4, 1, '#hashtag4'),
+       (NOW(), 'nickname15', 500, false, 4, 'university5', 5000, 5, 2, '#hashtag5'),
+       (NOW(), 'nickname16', 600, true, 5, 'university6', 6000, 6, 3, '#hashtag6'),
+       (NOW(), 'nickname17', 700, false, 6, 'university7', 7000, 7, 1, '#hashtag7'),
+       (NOW(), 'nickname18', 800, true, 7, 'university8', 8000, 8, 2, '#hashtag8'),
+       (NOW(), 'nickname19', 900, false, 8, 'university9', 9000, 9, 3, '#hashtag9'),
+       (NOW(), 'nickname20', 1000, true, 9, 'university10', 10000, 10, 1, '#hashtag10');
 
 -- Using UNION ALL to generate numbers up to 280
 WITH RECURSIVE numbers(val) AS (
@@ -59,7 +58,7 @@ WITH RECURSIVE numbers(val) AS (
     WHERE val < 280
 )
 
-INSERT INTO post (created_at, nickname, popularity, published, report_count, university, view_count, post_point, image_id, member_id, hashtag)
+INSERT INTO post (created_at, nickname, popularity, published, report_count, university, view_count, image_id, member_id, hashtag)
 SELECT
     NOW(),
     'nickname' || (val + 20),
@@ -68,7 +67,6 @@ SELECT
     0,
     'university' || ((val % 10) + 1),
     (val * 1000),
-    400,
     ((val % 10) + 1),
     ((val % 3) + 1),
     '#hashtag' || ((val % 10) + 1)
