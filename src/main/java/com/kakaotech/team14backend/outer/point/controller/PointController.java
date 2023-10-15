c50531b265dad8d76d6e1cf3a5f7e153fee4cc48
hwangdaesun <hds650588@gmail.com>
2023-10-15 13:17:09 +0900

    feat : 요청이 정상이고, 포인트가 부족하지 않은 경우 인스타 아이디 반환

--- src/main/java/com/kakaotech/team14backend/outer/point/controller/PointController.java
+++ src/main/java/com/kakaotech/team14backend/outer/point/controller/PointController.java
@@ -3,6 +3,7 @@ package com.kakaotech.team14backend.outer.point.controller;
 import com.kakaotech.team14backend.common.ApiResponse;
 import com.kakaotech.team14backend.common.ApiResponseGenerator;
 import com.kakaotech.team14backend.outer.point.dto.UsePointByPopularPostRequestDTO;
+import com.kakaotech.team14backend.outer.point.dto.UsePointByPopularPostResponseDTO;
 import com.kakaotech.team14backend.outer.point.service.PointService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -21,9 +22,10 @@ public class PointController {
 
   @ApiOperation(value = "인기 피드 게시물 포인트 사용")
   @PostMapping("/point")
-  public ApiResponse<ApiResponse.CustomBody<Void>> usePointByPopularPost(@RequestBody UsePointByPopularPostRequestDTO usePointByPopularPostRequestDTO) {
+  public ApiResponse<ApiResponse.CustomBody<UsePointByPopularPostResponseDTO>> usePointByPopularPost(@RequestBody UsePointByPopularPostRequestDTO usePointByPopularPostRequestDTO) {
     Long memberId = 1L;
-    pointService.usePointByPopularPost(usePointByPopularPostRequestDTO, memberId);
-    return ApiResponseGenerator.success(HttpStatus.OK);
+    String instaId = pointService.usePointByPopularPost(usePointByPopularPostRequestDTO, memberId);
+    UsePointByPopularPostResponseDTO usePointByPopularPostResponseDTO = new UsePointByPopularPostResponseDTO(instaId);
+    return ApiResponseGenerator.success(usePointByPopularPostResponseDTO,HttpStatus.OK);
   }
 }
