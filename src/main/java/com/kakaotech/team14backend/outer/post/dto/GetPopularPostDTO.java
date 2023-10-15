da489a4a9e01ad99f8ea1c3d945241da6f350a07
hwangdaesun <hds650588@gmail.com>
2023-10-15 12:46:28 +0900

    refactor : postPoint의 타입 Long으로 변경

--- src/main/java/com/kakaotech/team14backend/outer/post/dto/GetPopularPostDTO.java
+++ src/main/java/com/kakaotech/team14backend/outer/post/dto/GetPopularPostDTO.java
@@ -2,5 +2,5 @@ package com.kakaotech.team14backend.outer.post.dto;
 
 import java.util.List;
 
-public record GetPopularPostDTO(Long postId, String imageUri, List<String> hashTags, Long likeCount, Integer postPoint, String nickname, Integer postLevel) {
+public record GetPopularPostDTO(Long postId, String imageUri, List<String> hashTags, Long likeCount, Long postPoint, String nickname, Integer postLevel) {
 }
