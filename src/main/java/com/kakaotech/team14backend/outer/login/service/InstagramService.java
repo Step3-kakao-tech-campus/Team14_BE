f64cafc05fcd6518e5b120e429917f0bb570fe7e
yuseonkim <yusun715@icloud.com>
2023-10-16 11:13:32 +0900

    feat : 서비스 및 컨트롤러 클래스 수정
    변경된 점을 적용하기 위해 수정하였습니다.
    
    이슈번호 : #102

--- src/main/java/com/kakaotech/team14backend/outer/login/service/InstagramService.java
+++ src/main/java/com/kakaotech/team14backend/outer/login/service/InstagramService.java
@@ -1,13 +1,13 @@
 package com.kakaotech.team14backend.outer.login.service;
 
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.kakaotech.team14backend.common.ApiResponse;
 import com.kakaotech.team14backend.common.ApiResponseGenerator;
 import com.kakaotech.team14backend.inner.member.model.Member;
 import com.kakaotech.team14backend.inner.member.model.Role;
 import com.kakaotech.team14backend.inner.member.repository.MemberRepository;
+import com.kakaotech.team14backend.jwt.dto.TokenDTO;
 import com.kakaotech.team14backend.jwt.service.TokenService;
-import com.kakaotech.team14backend.outer.login.dto.InstagramAccessTokenDto;
+import com.kakaotech.team14backend.outer.login.dto.KakaoProfileDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
@@ -16,9 +16,9 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -26,32 +26,24 @@ public class InstagramService {
 
   private final String GRANT_TYPE = "authorization_code";
 
-  @Value("${instagram.connect-url}")
-  private String InstagramConnectURL;
-
-  @Value("${instagram.client-id}")
+  @Value("${oauth2.instagram.client-id}")
   private String CLIENT_ID;
-  @Value("${instagram.client-secret}")
+  @Value("${oauth2.instagram.client-secret}")
   private String CLIENT_SECRET;
 
-  @Value("${instagram.redirect-uri}")
+  @Value("${oauth2.instagram.redirect-uri}")
   private String REDIRECT_URI;
-  @Value("${instagram.token-url}")
+  @Value("${oauth2.instagram.token-url}")
   private String TOKEN_URL;
-  @Value("${instagram.user-info-url}")
+  @Value("${oauth2.instagram.user-info-url}")
   private String USER_INFO_URL;
+  @Value("${jwt.token-validity-in-seconds-refreshToken}")
+  private int cookieAge;
 
   private final MemberRepository memberRepository;
-  private final TokenMemoryStorageService tokenMemoryStorageService;
   private final TokenService tokenService;
 
-  public InstagramAccessTokenDto getAccessToken(HttpServletRequest request, String code) {
-    String jwt = request.getHeader("accessToken");
-    UUID uuid = UUID.randomUUID();
-    //자동 콜백 리다이렉션 페이지로의 이동을 고려해 토큰을 서버측에 임시 저장
-    //사용자 식별을 위해 uuid로 바꿔 key값 저장
-    String uniqueKey = uuid.toString();
-    tokenMemoryStorageService.saveJwtToken(uniqueKey,jwt);
+  public String getAccessToken(String code) {
 
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -71,37 +63,47 @@ public class InstagramService {
     ResponseEntity<Map> responseEntity = restTemplate.postForEntity(accessTokenRequestUrl, requestMap, Map.class);
     // 응답 받은 JSON 데이터 반환
     String accessToken = (String) responseEntity.getBody().get("access_token");
-    InstagramAccessTokenDto instagramAccessTokenDto = new InstagramAccessTokenDto(uniqueKey,accessToken);
-    return instagramAccessTokenDto;
+    return accessToken;
   }
 
-  public ApiResponse<?> getUserInfo(InstagramAccessTokenDto instagramAccessTokenDto) {
+  public void getInstagramAndSetNewToken(String kakaoId, String accessToken) {
 
-    String accessToken = tokenMemoryStorageService.getJwtToken(instagramAccessTokenDto.getAccessToken());
-    String uniqueKey = instagramAccessTokenDto.getUniqueKey();
     RestTemplate restTemplate = new RestTemplate();
 
     // 응답 받은 JSON 데이터 반환
     String userInfoUrl = USER_INFO_URL + "&access_token=" + accessToken;
-    try {
-      ResponseEntity<Map> userResponse = restTemplate.exchange(userInfoUrl, HttpMethod.GET, null, Map.class);
-      String instaId = (String) userResponse.getBody().get("username");
-      String jwt = tokenMemoryStorageService.getJwtToken(uniqueKey);
-      DecodedJWT decodedJWT = TokenService.verifyToken(jwt);
-      String kakaoId = decodedJWT.getClaim("kakaoId").asString();
-      tokenMemoryStorageService.removeJwtToken(uniqueKey);
-      Member member = memberRepository.findByKakaoId(kakaoId);
-      member.updateInstagram(Role.ROLE_BEGINNER,instaId);
-    }catch(NullPointerException e){
-      return ApiResponseGenerator.fail("401","잘못된 접근입니다.",HttpStatus.FORBIDDEN);
-    }
+    ResponseEntity<Map> userResponse = restTemplate.exchange(userInfoUrl, HttpMethod.GET, null, Map.class);
+    String instaId = (String) userResponse.getBody().get("username");
+    Member memberEntity = memberRepository.findByKakaoId(kakaoId);
+    memberEntity.updateInstagram(Role.ROLE_BEGINNER, instaId);
+  }
+
+  public ApiResponse<?> connectInstagramSuccessHandler(HttpServletResponse response,String kakaoId){
+    Member member = memberRepository.findByKakaoId(kakaoId);
+    TokenDTO tokenDTO = tokenService.createOrUpdateToken(member);
+//    ApiResponse<?> apiResponse = new ApiResponse<>(new ApiResponse.CustomBody(true,"accessToken:" + jwt,null), HttpStatus.OK);
+
+    response.setContentType("application/json");
+    // 토큰을 HTTP 헤더에 추가
+    response.addHeader("Authorization", tokenDTO.getAccessToken());
+
+    // 리프레시토큰을 쿠키에 저장한다.
+    String refreshTokenCookieName = "RefreshToken";
+    String refreshTokenCookieValue = tokenDTO.getRefreshToken();
+    Cookie refreshTokenCookie = new Cookie("RefreshToken",refreshTokenCookieValue);
+    refreshTokenCookie.setHttpOnly(true);  //httponly 옵션 설정
+    refreshTokenCookie.setSecure(true); //https 옵션 설정
+    refreshTokenCookie.setPath("/"); // 모든 곳에서 쿠키열람이 가능하도록 설정
+    refreshTokenCookie.setMaxAge(cookieAge); //쿠키 만료시간 설정
+    response.addCookie(refreshTokenCookie);
+
+    //화면에 응답결과 출력
     return ApiResponseGenerator.success(HttpStatus.OK);
   }
 
-  public void changeUserRole(){
 
-  }
 }
+
 
 
 
