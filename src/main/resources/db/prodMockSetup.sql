04079c29953740b439219db5ce91faf252c00ddd
Beomsu <gobeumsu@gmail.com>
2023-10-22 16:19:41 +0900

    fix : university 필드 제거

--- src/main/resources/db/prodMockSetup.sql
+++ src/main/resources/db/prodMockSetup.sql
@@ -5,6 +5,7 @@ TRUNCATE TABLE post;
 TRUNCATE TABLE post_like_count;
 TRUNCATE TABLE point;
 SET foreign_key_checks=1;
+
 -- Member Table
 INSERT INTO member (created_at, insta_id, kakao_id,profile_image_url, total_like, updated_at, user_name, user_status,
                     role)
@@ -26,28 +27,28 @@ VALUES (NOW(), '/image/test.jpg'),
        (NOW(), '/image/test.jpg');
 
 -- Post Table
-INSERT INTO post (created_at, nickname, popularity, published, report_count, university, view_count,
+INSERT INTO post (created_at, nickname, popularity, published, report_count, view_count,
                   image_id, member_id, hashtag)
-VALUES (NOW(), 'nickname1', 100, true, 0, 'university1', 1000, 1, 1, '#hashtag1'),
-       (NOW(), 'nickname2', 200, true, 1, 'university2', 2000, 2, 2, '#hashtag2'),
-       (NOW(), 'nickname3', 300, false, 2, 'university3', 3000, 3, 3, '#hashtag3'),
-       (NOW(), 'nickname4', 400, true, 3, 'university4', 4000, 4, 1, '#hashtag4'),
-       (NOW(), 'nickname5', 500, false, 4, 'university5', 5000, 5, 2, '#hashtag5'),
-       (NOW(), 'nickname6', 600, true, 5, 'university6', 6000, 6, 3, '#hashtag6'),
-       (NOW(), 'nickname7', 700, false, 6, 'university7', 7000, 7, 1, '#hashtag7'),
-       (NOW(), 'nickname8', 800, true, 7, 'university8', 8000, 8, 2, '#hashtag8'),
-       (NOW(), 'nickname9', 900, false, 8, 'university9', 9000, 9, 3, '#hashtag9'),
-       (NOW(), 'nickname10', 1000, true, 9, 'university10', 10000, 10, 1, '#hashtag10'),
-       (NOW(), 'nickname11', 100, true, 0, 'university1', 1000, 1, 1, '#hashtag1'),
-       (NOW(), 'nickname12', 200, true, 1, 'university2', 2000, 2, 2, '#hashtag2'),
-       (NOW(), 'nickname13', 300, false, 2, 'university3', 3000, 3, 3, '#hashtag3'),
-       (NOW(), 'nickname14', 400, true, 3, 'university4', 4000, 4, 1, '#hashtag4'),
-       (NOW(), 'nickname15', 500, false, 4, 'university5', 5000, 5, 2, '#hashtag5'),
-       (NOW(), 'nickname16', 600, true, 5, 'university6', 6000, 6, 3, '#hashtag6'),
-       (NOW(), 'nickname17', 700, false, 6, 'university7', 7000, 7, 1, '#hashtag7'),
-       (NOW(), 'nickname18', 800, true, 7, 'university8', 8000, 8, 2, '#hashtag8'),
-       (NOW(), 'nickname19', 900, false, 8, 'university9', 9000, 9, 3, '#hashtag9'),
-       (NOW(), 'nickname20', 1000, true, 9, 'university10', 10000, 10, 1, '#hashtag10');
+VALUES (NOW(), 'nickname1', 100, true, 0, 1000, 1, 1, '#hashtag1'),
+       (NOW(), 'nickname2', 200, true, 1, 2000, 2, 2, '#hashtag2'),
+       (NOW(), 'nickname3', 300, false, 2, 3000, 3, 3, '#hashtag3'),
+       (NOW(), 'nickname4', 400, true, 3, 4000, 4, 1, '#hashtag4'),
+       (NOW(), 'nickname5', 500, false, 4, 5000, 5, 2, '#hashtag5'),
+       (NOW(), 'nickname6', 600, true, 5, 6000, 6, 3, '#hashtag6'),
+       (NOW(), 'nickname7', 700, false, 6, 7000, 7, 1, '#hashtag7'),
+       (NOW(), 'nickname8', 800, true, 7, 8000, 8, 2, '#hashtag8'),
+       (NOW(), 'nickname9', 900, false, 8, 9000, 9, 3, '#hashtag9'),
+       (NOW(), 'nickname10', 1000, true, 9, 10000, 10, 1, '#hashtag10'),
+       (NOW(), 'nickname11', 100, true, 0, 1000, 1, 1, '#hashtag1'),
+       (NOW(), 'nickname12', 200, true, 1, 2000, 2, 2, '#hashtag2'),
+       (NOW(), 'nickname13', 300, false, 2, 3000, 3, 3, '#hashtag3'),
+       (NOW(), 'nickname14', 400, true, 3, 4000, 4, 1, '#hashtag4'),
+       (NOW(), 'nickname15', 500, false, 4, 5000, 5, 2, '#hashtag5'),
+       (NOW(), 'nickname16', 600, true, 5, 6000, 6, 3, '#hashtag6'),
+       (NOW(), 'nickname17', 700, false, 6, 7000, 7, 1, '#hashtag7'),
+       (NOW(), 'nickname18', 800, true, 7, 8000, 8, 2, '#hashtag8'),
+       (NOW(), 'nickname19', 900, false, 8, 9000, 9, 3, '#hashtag9'),
+       (NOW(), 'nickname20', 1000, true, 9, 10000, 10, 1, '#hashtag10');
 
 -- Using UNION ALL to generate numbers up to 280
 WITH RECURSIVE numbers(val) AS (
@@ -59,20 +60,18 @@ WITH RECURSIVE numbers(val) AS (
 )
 
 INSERT INTO post
-(created_at, nickname, popularity, published, report_count, university, view_count, image_id, member_id, hashtag)
+(created_at, nickname, popularity, published, report_count, view_count, image_id, member_id, hashtag)
 SELECT
     NOW(),
     'nickname' || (val + 20),
     (val * 100),
     CASE WHEN val % 2 = 0 THEN true ELSE false END,
     0,
-    'university' || ((val % 10) + 1),
     (val * 1000),
     ((val % 10) + 1),
     ((val % 3) + 1),
     '#hashtag' || ((val % 10) + 1)
 FROM numbers;
-
 
 -- Insert PostLikeCount for all the 300 posts
 INSERT INTO post_like_count
