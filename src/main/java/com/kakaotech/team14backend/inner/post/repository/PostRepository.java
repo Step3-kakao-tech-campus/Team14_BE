0fecf8f07c6d5fd38c66a11f82ffa3578c7c6681
Beomsu <gobeumsu@gmail.com>
2023-10-23 17:16:06 +0900

    fix : lastPostId 기준으로 다음 게시물을 가져오도록

--- src/main/java/com/kakaotech/team14backend/inner/post/repository/PostRepository.java
+++ src/main/java/com/kakaotech/team14backend/inner/post/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.kakaotech.team14backend.inner.post.repository;
 
 import com.kakaotech.team14backend.inner.post.model.Post;
 import com.kakaotech.team14backend.outer.post.dto.GetIncompletePopularPostDTO;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,17 +11,21 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-  List<Post> findByPostIdGreaterThan(Long lastPostId, Pageable pageable);
 
-  @Query("SELECT new com.kakaotech.team14backend.outer.post.dto.GetIncompletePopularPostDTO(p.postId,i.imageUri,p.hashtag,pl.likeCount,p.popularity,p.nickname) " +
-      "FROM Post p " +
-      "JOIN p.image i " + // Image에 대한 JOIN
-      "JOIN p.postLikeCount pl " + // PostLike에 대한 JOIN
-      "ORDER BY p.popularity DESC")
+  @Query(
+      "SELECT new com.kakaotech.team14backend.outer.post.dto.GetIncompletePopularPostDTO(p.postId,i.imageUri,p.hashtag,pl.likeCount,p.popularity,p.nickname) "
+          +
+          "FROM Post p " +
+          "JOIN p.image i " + // Image에 대한 JOIN
+          "JOIN p.postLikeCount pl " + // PostLike에 대한 JOIN
+          "ORDER BY p.popularity DESC")
   List<GetIncompletePopularPostDTO> findTop300ByOrderByPopularityDesc(Pageable pageable);
 
   @Query("SELECT p FROM Post p WHERE p.member.memberId = :memberId")
   List<Post> findByMemberId(Long memberId, Pageable pageable);
+
+  @Query("SELECT p FROM Post p WHERE p.postId > :lastPostId ORDER BY p.postId asc ")
+  List<Post> findNextPosts(@Param("lastPostId") Long lastPostId, Pageable pageable);
 
   @Query("SELECT p FROM Post p WHERE p.member.memberId = :memberId AND p.postId > :lastPostId")
   List<Post> findByMemberIdAndPostIdGreaterThan(Long memberId, Long lastPostId, Pageable pageable);
--- src/main/java/com/kakaotech/team14backend/inner/post/usecase/FindPostListUsecase.java
+++ src/main/java/com/kakaotech/team14backend/inner/post/usecase/FindPostListUsecase.java
@@ -8,8 +8,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,7 +22,7 @@ public class FindPostListUsecase {
    * 홈 피드의 게시글들을 가져오는 유즈케이스. lastPostId와 size를 사용하여 페이지네이션을 지원합니다.
    */
   public GetPostListResponseDTO excute(Long lastPostId, int size) {
-    Pageable pageable = PageRequest.of(0, size + 1, Sort.by(Direction.DESC, "postId"));
+    Pageable pageable = PageRequest.of(0, size+1);  // 첫 페이지에 대한 요청, 페이지 크기는 10
 
     List<Post> postList;
 
@@ -33,16 +31,17 @@ public class FindPostListUsecase {
     if (lastPostId == null) {
       postList = postRepository.findAll(pageable).getContent();
     } else {
-      postList = postRepository.findByPostIdGreaterThan(lastPostId, pageable);
+      postList = postRepository.findNextPosts(lastPostId, pageable);
     }
-
+    System.out.println("postList = " + postList);
     boolean hasNext = false;
     // 가져온 게시물 수가 요청된 크기보다 큰지 확인
     // 참이면 hasNext 플래그를 true로 설정하고 목록을 요청된 크기로 자름
     if (postList.size() > size) {
       hasNext = true;
-      postList.subList(0, size);
+      postList = postList.subList(0, size);  // postList를 업데이트하려면 할당이 필요합니다.
     }
+
     return new GetPostListResponseDTO(PostMapper.from(postList), hasNext);
   }
 }
