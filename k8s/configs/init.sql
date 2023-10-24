e0fc58370f44a62cb75a71ae45e977b0056af275
Beomsu <gobeumsu@gmail.com>
2023-10-24 20:15:07 +0900

    fix : init sql

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -7,3 +7,54 @@ GRANT ALL
     ON krampoline.* TO 'root'@'localhost';
 FLUSH
     PRIVILEGES;
+
+-- image 테이블
+CREATE TABLE image
+(
+    image_id   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    image_uri  VARCHAR(100)                        NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+-- member 테이블
+CREATE TABLE member
+(
+    member_id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_name         VARCHAR(50)                                                     NOT NULL,
+    kakao_id          VARCHAR(50)                                                     NOT NULL,
+    insta_id          VARCHAR(50)                                                     NOT NULL,
+    profile_image_url TEXT                                                            NOT NULL,
+    role              VARCHAR(50)                                                     NOT NULL,
+    total_like        BIGINT                                                          NOT NULL DEFAULT 0,
+    created_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP                             NOT NULL,
+    updated_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    user_status       VARCHAR(20)                                                     NOT NULL
+);
+
+-- post 테이블
+CREATE TABLE post
+(
+    post_id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id    BIGINT,
+    image_id     BIGINT,
+    nickname     VARCHAR(50)                         NOT NULL,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    published    BOOLEAN                             NOT NULL,
+    hashtag      VARCHAR(30),
+    view_count   BIGINT,
+    popularity   BIGINT,
+    report_count INT                                 NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (member_id),
+    FOREIGN KEY (image_id) REFERENCES image (image_id)
+);
+
+-- post_like 테이블
+CREATE TABLE post_like
+(
+    post_like_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id    BIGINT,
+    post_id      BIGINT,
+    is_liked     BOOLEAN,
+    FOREIGN KEY (member_id) REFERENCES member (member_id),
+    FOREIGN KEY (post_id) REFERENCES post (post_id)
+);
