306f6fe5170c6100a9b62854748bb2dcf041875a
Beomsu <gobeumsu@gmail.com>
2023-10-24 20:08:36 +0900

    fix : table entity 변경 사항 반영

--- src/main/java/com/kakaotech/team14backend/inner/post/repository/PostRepository.java
+++ src/main/java/com/kakaotech/team14backend/inner/post/repository/PostRepository.java
@@ -15,18 +15,18 @@ public interface PostRepository extends JpaRepository<Post, Long> {
   @Query(
       "SELECT new com.kakaotech.team14backend.outer.post.dto.GetIncompletePopularPostDTO(p.postId,i.imageUri,p.hashtag,pl.likeCount,p.popularity,p.nickname) "
           +
-          "FROM Post p " +
+          "FROM post p " +
           "JOIN p.image i " + // Image에 대한 JOIN
           "JOIN p.postLikeCount pl " + // PostLike에 대한 JOIN
           "ORDER BY p.popularity DESC")
   List<GetIncompletePopularPostDTO> findTop300ByOrderByPopularityDesc(Pageable pageable);
 
-  @Query("SELECT p FROM Post p WHERE p.member.memberId = :memberId")
+  @Query("SELECT p FROM post p WHERE p.member.memberId = :memberId")
   List<Post> findByMemberId(Long memberId, Pageable pageable);
 
-  @Query("SELECT p FROM Post p WHERE p.postId > :lastPostId ORDER BY p.postId asc ")
+  @Query("SELECT p FROM post p WHERE p.postId > :lastPostId ORDER BY p.postId asc ")
   List<Post> findNextPosts(@Param("lastPostId") Long lastPostId, Pageable pageable);
 
-  @Query("SELECT p FROM Post p WHERE p.member.memberId = :memberId AND p.postId > :lastPostId")
+  @Query("SELECT p FROM post p WHERE p.member.memberId = :memberId AND p.postId > :lastPostId")
   List<Post> findByMemberIdAndPostIdGreaterThan(Long memberId, Long lastPostId, Pageable pageable);
 }
