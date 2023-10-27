299601148cd935fe7da585e8b69ec64f53104a59
Beomsu <gobeumsu@gmail.com>
2023-10-27 17:15:32 +0900

    fix : record로 dto 변경 사항 반영

--- src/main/java/com/kakaotech/team14backend/outer/member/dto/GetMemberInfoResponseDTO.java
+++ src/main/java/com/kakaotech/team14backend/outer/member/dto/GetMemberInfoResponseDTO.java
@@ -1,7 +1,5 @@
 package com.kakaotech.team14backend.outer.member.dto;
 
-import lombok.Builder;
-
 public record GetMemberInfoResponseDTO(Long memberId, String userName, String kakaoId,
                                        Long totalLike,
                                        String profileImageUrl) {
