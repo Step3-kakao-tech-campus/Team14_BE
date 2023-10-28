d7bdbf26d1659fade1927e04816a3654a704733c
yuseonkim <yusun715@icloud.com>
2023-10-29 08:25:48 +0900

    feat : 재발급 기능 및 로그아웃 기능 수정
    재발급 기능의 오류메세지를 추가하였고,
    로그아웃 시 HTTPSONLY로 제공한 RefreshToken을 삭제하는 과정을 추가하였습니다.

--- src/main/java/com/kakaotech/team14backend/jwt/Controller/JwtController.java
+++ src/main/java/com/kakaotech/team14backend/jwt/Controller/JwtController.java
@@ -15,10 +15,6 @@ import com.kakaotech.team14backend.jwt.service.TokenService;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,7 +37,7 @@ public class JwtController {
       // 새로운 액세스 토큰을 HTTP 헤더에 추가
       response.addHeader("Authorization", reissueDTO.getAccessToken());
       return ApiResponseGenerator.success(HttpStatus.OK);
-    } catch (NullPointerException | TokenExpiredException e) {
+    } catch (NullPointerException | TokenExpiredException | TokenValidationException e) {
       // 쿠키에 리프레시토큰이 없을 시 혹은 리프레시토큰 검증 실패 시 로그인필요 에러메세지 전달
       return ApiResponseGenerator.fail("401","리프레시토큰이 유효하지 않습니다.",HttpStatus.UNAUTHORIZED);
     }
