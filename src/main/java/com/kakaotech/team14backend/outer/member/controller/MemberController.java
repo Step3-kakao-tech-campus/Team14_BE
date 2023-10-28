f06d6314292586e521b7bba46592ebcd45490b94
Beomsu <gobeumsu@gmail.com>
2023-10-28 10:35:49 +0900

    feat : 유저 정보에 따라 로그인 정보 출력하도록 변경

--- src/main/java/com/kakaotech/team14backend/outer/member/controller/MemberController.java
+++ src/main/java/com/kakaotech/team14backend/outer/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.kakaotech.team14backend.outer.member.controller;
 
+import com.kakaotech.team14backend.auth.PrincipalDetails;
 import com.kakaotech.team14backend.common.ApiResponse;
 import com.kakaotech.team14backend.common.ApiResponseGenerator;
 import com.kakaotech.team14backend.outer.member.dto.GetMemberInfoResponseDTO;
@@ -7,6 +8,7 @@ import com.kakaotech.team14backend.outer.member.service.MemberService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +22,9 @@ public class MemberController {
 
   @ApiOperation(value = "마이페이지 계정 상세 조회")
   @GetMapping("/user/info")
-  public ApiResponse<ApiResponse.CustomBody<GetMemberInfoResponseDTO>> getMyPageInfo() {
-    Long memberId = 1L;
-
+  public ApiResponse<ApiResponse.CustomBody<GetMemberInfoResponseDTO>> getMyPageInfo(
+      @AuthenticationPrincipal PrincipalDetails principalDetails) {
+    Long memberId = principalDetails.getMember().getMemberId();
     GetMemberInfoResponseDTO myPageInfo = memberService.getMyPageInfo(memberId);
     return ApiResponseGenerator.success(myPageInfo, HttpStatus.OK);
   }
