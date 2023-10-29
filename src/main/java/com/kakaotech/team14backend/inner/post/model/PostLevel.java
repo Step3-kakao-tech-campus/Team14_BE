8b5e6d03de0a5155bee4fdd501cbdedd565e5541
Sonny <hds650588@gmail.com>
2023-10-29 13:00:27 +0900

    refactor :  PostLevel의 start와 end속성 1~30등 안으로 수정 및 리팩토링

--- src/main/java/com/kakaotech/team14backend/inner/post/model/PostLevel.java
+++ src/main/java/com/kakaotech/team14backend/inner/post/model/PostLevel.java
@@ -1,32 +1,34 @@
 package com.kakaotech.team14backend.inner.post.model;
 
 public enum PostLevel {
-  LV3_RANGE_START(1),
-  LV3_RANGE_END(29),
-  LV2_RANGE_START(30),
-  LV2_RANGE_END(99),
-  LV1_RANGE_START(100),
-  LV1_RANGE_END(299);
-  private int value;
 
-  public int getValue() {
-    return value;
+  LV3(1, 10),
+  LV2(11, 20),
+  LV1(21, 30),
+  ;
+  private final int start;
+  private final int end;
+
+  PostLevel(final int start, final int end) {
+    this.start = start;
+    this.end = end;
   }
 
-  PostLevel(int value) {
-    this.value = value;
+  public static PostLevel from(final int level) {
+    return switch (level) {
+      case 1 -> LV1;
+      case 2 -> LV2;
+      case 3 -> LV3;
+      default -> throw new IllegalArgumentException("Invalid level: %d".formatted(level));
+    };
   }
 
-  public static int[] getLevelRange(int postLevel) {
-    if(postLevel == 3) {
-      return new int[] {1, 29};
-    } else if(postLevel == 2) {
-      return new int[] {30, 99};
-    } else if(postLevel == 1) {
-      return new int[] {100, 299};
-    } else {
-      throw new IllegalArgumentException("Invalid level: " + postLevel);
-    }
+  public int start() {
+    return start;
+  }
+
+  public int end() {
+    return end;
   }
 
 }
