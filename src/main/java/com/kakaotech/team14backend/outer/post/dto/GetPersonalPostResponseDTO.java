a58f4fa7244b4be8a08f52b8ff439cf5f126d308
Beomsu <gobeumsu@gmail.com>
2023-10-31 10:56:57 +0900

    fix: typo

--- src/main/java/com/kakaotech/team14backend/outer/post/dto/GetPersonalPostResponseDTO.java
+++ src/main/java/com/kakaotech/team14backend/outer/post/dto/GetPersonalPostResponseDTO.java
@@ -1,6 +1,6 @@
 package com.kakaotech.team14backend.outer.post.dto;
 
-public record GetPersonalPostResponseDTO(Long postid, String imageUri, String nickname,
+public record GetPersonalPostResponseDTO(Long postId, String imageUri, String nickname,
                                          String createdAt, Long viewCount, Long likeCount) {
 
 //  {
