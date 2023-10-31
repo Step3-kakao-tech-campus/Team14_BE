fa2a855643af8f310278de0756b90b45a56db00e
Beomsu <gobeumsu@gmail.com>
2023-10-31 15:41:41 +0900

    refactor : DTO 인터페이스 상속 받아 사용하도록 변경

--- src/main/java/com/kakaotech/team14backend/outer/post/dto/GetHomePostListResponseDTO.java
+++ src/main/java/com/kakaotech/team14backend/outer/post/dto/GetHomePostListResponseDTO.java
@@ -2,7 +2,8 @@ package com.kakaotech.team14backend.outer.post.dto;
 
 import java.util.List;
 
-public record GetHomePostListResponseDTO(Long lastPostId, List<GetPostResponseDTO> postList,
+public record GetHomePostListResponseDTO(Long lastPostId,
+                                         List<? extends GetHomePostInterface> postList,
                                          Boolean hasNext) {
 
 }
