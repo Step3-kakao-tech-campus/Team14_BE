460c1a6115f6312fd8568e273300c43aeef2e2e2
Beomsu <gobeumsu@gmail.com>
2023-11-02 14:18:51 +0900

    feat : userInfo 해당 유저의 받은 좋아요 수 반영

--- src/main/java/com/kakaotech/team14backend/outer/member/service/MemberService.java
+++ src/main/java/com/kakaotech/team14backend/outer/member/service/MemberService.java
@@ -5,8 +5,13 @@ import com.kakaotech.team14backend.inner.member.model.Role;
 import com.kakaotech.team14backend.inner.member.model.Status;
 import com.kakaotech.team14backend.inner.member.repository.MemberRepository;
 import com.kakaotech.team14backend.inner.point.usecase.CreatePointUsecase;
+import com.kakaotech.team14backend.inner.post.model.Post;
+import com.kakaotech.team14backend.inner.post.model.PostLikeCount;
+import com.kakaotech.team14backend.inner.post.repository.PostLikeCountRepository;
 import com.kakaotech.team14backend.outer.member.dto.GetMemberInfoResponseDTO;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +24,8 @@ public class MemberService {
 
   //private final CreateMemberUsecase createMemberUsecase;
   private final MemberRepository memberRepository; // 추후 Usecase 로 변경
+  private final PostLikeCountRepository postLikeCountRepository;
+  private final PostLikeCount postLikeCount;
 
   public static Member createMember(String userName, String kakaoId, String instaId,
                                     String profileImageUrl, Role role, Long totalLike,
@@ -32,15 +39,24 @@ public class MemberService {
 //    createPointUsecase.execute(member, PointPolicy.GET_100_WHEN_SIGN_UP);
 //  }
 
-// todo : createMemberusease를 이용해 주세요! ++ 주석 참고
-
+  // todo : createMemberusease를 이용해 주세요! ++ 주석 참고
+  // todo : 전체 좋아요 수 발행하는 것들 event driven으로 변경 할
   public GetMemberInfoResponseDTO getMyPageInfo(Long memberId) {
-    Optional<Member> member = memberRepository.findById(memberId);
-    if (member.isEmpty()) {
-      throw new IllegalArgumentException("존재하지 않는 회원입니다.");
-    }
-    return new GetMemberInfoResponseDTO(member.get().getMemberId(), member.get().getUserName(),
-        member.get().getKakaoId(),member.get().getInstaId(), member.get().getTotalLike(), member.get().getProfileImageUrl());
+    // 회원 정보를 조회하고, 없다면 예외를 발생시킵니다.
+    Member member = memberRepository.findById(memberId)
+        .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+    // 회원이 작성한 게시물의 ID 목록을 스트림을 이용해 가져옵니다.
+    List<Long> postIds = member.getPosts().stream().map(Post::getPostId)
+        .collect(Collectors.toList());
+
+    // 좋아요의 총합을 스트림을 이용하여 계산합니다.
+    Long totalLike = postIds.stream().map(postLikeCountRepository::findByPostId)
+        .mapToLong(PostLikeCount::getLikeCount).sum();
+
+    // DTO를 생성하여 반환합니다.
+    return new GetMemberInfoResponseDTO(member.getMemberId(), member.getUserName(),
+        member.getKakaoId(), member.getInstaId(), totalLike, member.getProfileImageUrl());
   }
 
   @Transactional
