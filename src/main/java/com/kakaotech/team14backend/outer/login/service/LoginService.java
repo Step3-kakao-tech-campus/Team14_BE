79504d07f26fee67a250a41886dc9b2e4311900f
yuseonkim <yusun715@icloud.com>
2023-11-04 03:14:44 +0900

    feat : HttpClientException에 대한 핸들러 설정
    카카오 OAauth2 관련하여 요청 중 에러 발생 시 어떻게 처리해야할까 고민하다, 해당 에러 발생 시 응답스크립트를 클라이언트에게 보여주게 바꿨습니다.

--- src/main/java/com/kakaotech/team14backend/outer/login/service/LoginService.java
+++ src/main/java/com/kakaotech/team14backend/outer/login/service/LoginService.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kakaotech.team14backend.auth.PrincipalDetails;
-import com.kakaotech.team14backend.common.ApiResponse;
-import com.kakaotech.team14backend.common.ApiResponseGenerator;
 import com.kakaotech.team14backend.inner.member.model.Member;
 import com.kakaotech.team14backend.inner.member.model.Role;
 import com.kakaotech.team14backend.inner.member.model.Status;
@@ -22,7 +20,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -43,7 +40,7 @@ public class LoginService {
   private int cookieAge;
 
   @Value("${oauth2.kakao.redirect-uri}")
-  private  String KAKAO_REDIRECT_URI;
+  private String KAKAO_REDIRECT_URI;
   @Value("${oauth2.kakao.client-id}")
   private String KAKAO_CLIENT_ID;
   @Value("${oauth2.kakao.user-info-uri}")
@@ -69,9 +66,10 @@ public class LoginService {
   }
 
 
-  public String getKaKaoAccessToken(String code){
+  public String getKaKaoAccessToken(String code) {
     HttpHeaders headers = new HttpHeaders();
     headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+    RestTemplate rt = new RestTemplate();
 
     MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
     params.add("grant_type", "authorization_code");
@@ -83,7 +81,7 @@ public class LoginService {
         new HttpEntity<>(params, headers);
 
     //Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음.
-    ResponseEntity<String> response = proxyRestTemplate.exchange(
+    ResponseEntity<String> response = rt.exchange(
         KAKAO_TOKEN_URI,
         HttpMethod.POST,
         kakaoTokenRequest,
@@ -94,57 +92,43 @@ public class LoginService {
     GetKakaoOauth2TokenDTO oAuthToken = null;
     try {
       oAuthToken = objectMapper.readValue(response.getBody(), GetKakaoOauth2TokenDTO.class);
-    } catch(JsonProcessingException e){
-      e.printStackTrace();;
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      ;
     }
     return oAuthToken.getAccess_token();
   }
 
   public KakaoProfileDTO getKakaoUserInfo(String AccessToken) throws IOException {
     HttpHeaders headers = new HttpHeaders();
-
-//    Proxy proxy = new Proxy(Proxy.Type.HTTP,new InetSocketAddress("krmp-proxy.9rum.cc", 3128));
-//    SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-//    requestFactory.setProxy(proxy);
-//    RestTemplate restTemplate = new RestTemplate(requestFactory);
-
-    headers.add("Authorization","Bearer "+ AccessToken);
+    headers.add("Authorization", "Bearer " + AccessToken);
     headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-    HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest =
-        new HttpEntity<>(headers);
-
+    RestTemplate rt = new RestTemplate();
+    HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers);
     //Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음.
-    ResponseEntity<String> response = proxyRestTemplate.exchange(
+    ResponseEntity<String> response = rt.exchange(
         KAKAO_USER_INFO_URI,
         HttpMethod.POST,
         kakaoProfileRequest,
         String.class
     );
-
-    ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);;
-    KakaoProfileDTO kakaoProfileDTO = null;
-    try {
-      kakaoProfileDTO = objectMapper.readValue(response.getBody(), KakaoProfileDTO.class);
-    } catch(JsonProcessingException e){
-      e.printStackTrace();;
-    }
-
-    return kakaoProfileDTO;
+    ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    return objectMapper.readValue(response.getBody(), KakaoProfileDTO.class);
   }
 
+
   @Transactional
-  public void createOrLoginMember(KakaoProfileDTO kakaoProfileDTO){
+  public void createOrLoginMember(KakaoProfileDTO kakaoProfileDTO) {
     String kakaoId = kakaoProfileDTO.getId();
     String userName = kakaoProfileDTO.getProperties().getNickname();
     String profileImage = kakaoProfileDTO.getProperties().getProfileImage();
     Member memberEntity = memberRepository.findByKakaoId(kakaoId);
 
     if (memberEntity == null) {
-      memberEntity = memberService.createMember(userName, kakaoId,"none",profileImage, Role.ROLE_BEGINNER,0L, Status.STATUS_ACTIVE);
+      memberEntity = memberService.createMember(userName, kakaoId, "none", profileImage, Role.ROLE_BEGINNER, 0L, Status.STATUS_ACTIVE);
       memberRepository.save(memberEntity);
     }
-    if (memberEntity.getUserStatus().equals(Status.STATUS_INACTIVE)){
+    if (memberEntity.getUserStatus().equals(Status.STATUS_INACTIVE)) {
       memberService.makeUserActive(memberEntity.getMemberId());
     }
 
@@ -154,7 +138,8 @@ public class LoginService {
     SecurityContextHolder.getContext().setAuthentication(authentication);
   }
 
-  public ApiResponse<?> AuthenticationSuccessHandelr(HttpServletResponse response,KakaoProfileDTO kakaoProfileDTO){
+  public void AuthenticationSuccessHandelr(HttpServletResponse response, KakaoProfileDTO
+      kakaoProfileDTO) {
     String kakaoId = kakaoProfileDTO.getId();
     Member member = memberRepository.findByKakaoId(kakaoId);
     TokenDTO tokenDTO = tokenService.createOrUpdateToken(member);
@@ -166,14 +151,11 @@ public class LoginService {
     // 리프레시토큰을 쿠키에 저장한다.
     String refreshTokenCookieName = "RefreshToken";
     String refreshTokenCookieValue = tokenDTO.getRefreshToken();
-    Cookie refreshTokenCookie = new Cookie("RefreshToken",refreshTokenCookieValue);
+    Cookie refreshTokenCookie = new Cookie("RefreshToken", refreshTokenCookieValue);
     refreshTokenCookie.setHttpOnly(true);  //httponly 옵션 설정
     refreshTokenCookie.setSecure(true); //https 옵션 설정
     refreshTokenCookie.setPath("/"); // 모든 곳에서 쿠키열람이 가능하도록 설정
     refreshTokenCookie.setMaxAge(cookieAge); //쿠키 만료시간 설정
     response.addCookie(refreshTokenCookie);
-
-    //화면에 응답결과 출력
-    return ApiResponseGenerator.success(HttpStatus.OK);
   }
 }
