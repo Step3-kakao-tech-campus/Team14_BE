62d00d2d4a0adf87502849513148b81352e7a741
Sonny <hds650588@gmail.com>
2023-11-04 17:40:07 +0900

    refactor : postPoint 타입 변경

--- src/main/java/com/kakaotech/team14backend/outer/post/dto/GetPopularPostResponseDTO.java
+++ src/main/java/com/kakaotech/team14backend/outer/post/dto/GetPopularPostResponseDTO.java
@@ -2,5 +2,5 @@ package com.kakaotech.team14backend.outer.post.dto;
 
 import java.util.List;
 
-public record GetPopularPostResponseDTO(Long postId, String imageUrl, List<String> hashTags, Long likeCount, Integer postPoint, String nickname,Boolean isLiked, Integer postLevel) {
+public record GetPopularPostResponseDTO(Long postId, String imageUrl, List<String> hashTags, Long likeCount, Long postPoint, String nickname,Boolean isLiked, Integer postLevel) {
 }
