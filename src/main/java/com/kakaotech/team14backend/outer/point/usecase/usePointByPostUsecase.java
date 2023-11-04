d8bb66adfe39e72c469a2ba9dcb09cfd9e8104bb
Beomsu <gobeumsu@gmail.com>
2023-11-04 13:09:23 +0900

    refactor : 해당 포스트 작성자의 instaId 가져오도록 변경

--- src/main/java/com/kakaotech/team14backend/outer/point/usecase/usePointByPostUsecase.java
+++ src/main/java/com/kakaotech/team14backend/outer/point/usecase/usePointByPostUsecase.java
@@ -3,6 +3,8 @@ package com.kakaotech.team14backend.outer.point.usecase;
 import com.kakaotech.team14backend.inner.member.model.Member;
 import com.kakaotech.team14backend.inner.member.service.FindMemberService;
 import com.kakaotech.team14backend.inner.point.usecase.UsePointUsecase;
+import com.kakaotech.team14backend.inner.post.model.Post;
+import com.kakaotech.team14backend.inner.post.repository.PostRepository;
 import com.kakaotech.team14backend.outer.point.dto.UsePointByPostRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,15 +17,18 @@ public class usePointByPostUsecase {
 
   private final FindMemberService findMemberService;
   private final UsePointUsecase usePointUsecase;
+  private final PostRepository postRepository;
 
   //  private final Post
   public String getInstaId(UsePointByPostRequestDTO usePointByPostRequestDTO,
-      Long memberId) {
+      Long cureentMemberId) {
     Long postId = usePointByPostRequestDTO.postId();
-    Member member = findMemberService.execute(memberId);
+    Post post = postRepository.findById(postId).orElseThrow();
 
+    Member memberOfPost = post.getMember();
+    Member member = findMemberService.execute(cureentMemberId);
     final Long GET_POINT = 1L;
     usePointUsecase.execute(member, GET_POINT);
-    return member.getInstaId();
+    return memberOfPost.getInstaId();
   }
 }
