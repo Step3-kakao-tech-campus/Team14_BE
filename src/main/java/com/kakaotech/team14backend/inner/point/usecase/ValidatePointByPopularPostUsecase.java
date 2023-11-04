bce0acfe6996683c7d9592ab83e129556f9c52f9
Sonny <hds650588@gmail.com>
2023-11-04 17:48:28 +0900

    chore : 사용하지 않는 import문 제거

--- src/main/java/com/kakaotech/team14backend/inner/point/usecase/ValidatePointByPopularPostUsecase.java
+++ src/main/java/com/kakaotech/team14backend/inner/point/usecase/ValidatePointByPopularPostUsecase.java
@@ -2,22 +2,14 @@ package com.kakaotech.team14backend.inner.point.usecase;
 
 import com.kakaotech.team14backend.common.MessageCode;
 import com.kakaotech.team14backend.common.RedisKey;
-import com.kakaotech.team14backend.exception.Exception400;
 import com.kakaotech.team14backend.exception.PostNotFoundException;
-import com.kakaotech.team14backend.inner.post.model.Post;
 import com.kakaotech.team14backend.inner.post.model.PostLevel;
 import com.kakaotech.team14backend.outer.point.dto.UsePointByPopularPostRequestDTO;
-import com.kakaotech.team14backend.outer.post.dto.GetIncompletePopularPostDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-
-import javax.transaction.Transactional;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
