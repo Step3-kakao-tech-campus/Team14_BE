18faef488bca25fc729b55db09724a5d3e367338
Beomsu <gobeumsu@gmail.com>
2023-11-05 19:13:35 +0900

    fix : 포인트 사용을 위한 트랜잭션 읽기 해제

--- src/main/java/com/kakaotech/team14backend/outer/point/service/PointService.java
+++ src/main/java/com/kakaotech/team14backend/outer/point/service/PointService.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class PointService {
 
   private final ValidatePointByPopularPostUsecase validatePointByPopularPostUsecase;
