112cf77b9249e542f181c638f325e17c4e285753
Beomsu <gobeumsu@gmail.com>
2023-11-05 10:24:46 +0900

    fix : typo

--- src/main/java/com/kakaotech/team14backend/inner/point/repository/PointHistoryRepository.java
+++ src/main/java/com/kakaotech/team14backend/inner/point/repository/PointHistoryRepository.java
@@ -3,7 +3,7 @@ package com.kakaotech.team14backend.inner.point.repository;
 import com.kakaotech.team14backend.inner.point.model.PointHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PointHistroyRepository extends JpaRepository<PointHistory,Long> {
+public interface PointHistoryRepository extends JpaRepository<PointHistory,Long> {
 
 
 }
