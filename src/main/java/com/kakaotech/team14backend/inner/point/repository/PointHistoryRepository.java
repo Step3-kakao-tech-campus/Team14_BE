a27e207e62fffbd9d2a66187922858a737ba5789
Beomsu <gobeumsu@gmail.com>
2023-11-06 14:59:01 +0900

    refactor :  받은 폭죽이 없을 경우 0 반환하도록 수정

--- src/main/java/com/kakaotech/team14backend/inner/point/repository/PointHistoryRepository.java
+++ src/main/java/com/kakaotech/team14backend/inner/point/repository/PointHistoryRepository.java
@@ -1,12 +1,13 @@
 package com.kakaotech.team14backend.inner.point.repository;
 
 import com.kakaotech.team14backend.inner.point.model.PointHistory;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
 
-  @Query("select sum(ph.transferPoint) from point_history ph where ph.recieverId = :receivedId")
-  Long sumReceivedFireworksByReceivedId(Long receivedId);
-
+  @Query("select ph from point_history ph where ph.recieverId = :receivedId")
+  Optional<List<PointHistory>> receivedFireworksByReceivedId(Long receiverId);
 }
