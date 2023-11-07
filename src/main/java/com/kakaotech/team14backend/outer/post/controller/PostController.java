670d57f5393c9d3aa9fc7e505a4830c2dec33580
Sonny <hds650588@gmail.com>
2023-11-07 19:41:35 +0900

    chore : 사용하지 않는 thorws 및 import문 제거

--- src/main/java/com/kakaotech/team14backend/outer/post/controller/PostController.java
+++ src/main/java/com/kakaotech/team14backend/outer/post/controller/PostController.java
@@ -21,7 +21,6 @@ import com.kakaotech.team14backend.outer.post.dto.UploadPostDTO;
 import com.kakaotech.team14backend.outer.post.dto.UploadPostRequestDTO;
 import com.kakaotech.team14backend.outer.post.service.PostService;
 import io.swagger.annotations.ApiOperation;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -84,7 +83,7 @@ public class PostController {
   @PostMapping(value = "/post", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ApiResponse<ApiResponse.CustomBody<Void>> uploadPost(
       @ModelAttribute UploadPostRequestDTO uploadPostRequestDTO,
-      @AuthenticationPrincipal PrincipalDetails principalDetails) throws IOException {
+      @AuthenticationPrincipal PrincipalDetails principalDetails){
     UploadPostDTO uploadPostDTO = new UploadPostDTO(principalDetails.getMember(),
         uploadPostRequestDTO);
     postService.uploadPost(uploadPostDTO);
