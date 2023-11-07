4cc2254b0a436541f1178d611b7d32bbdd90960b
Beomsu <gobeumsu@gmail.com>
2023-11-07 16:07:51 +0900

    refactor : 사용하지 않는 받은 폭죽 수 필드 제거

--- src/main/java/com/kakaotech/team14backend/inner/member/usecase/FindMemberInfoUsecase.java
+++ src/main/java/com/kakaotech/team14backend/inner/member/usecase/FindMemberInfoUsecase.java
@@ -2,18 +2,13 @@ package com.kakaotech.team14backend.inner.member.usecase;
 
 import com.kakaotech.team14backend.inner.member.model.Member;
 import com.kakaotech.team14backend.inner.member.service.FindMemberService;
-import com.kakaotech.team14backend.inner.point.model.PointHistory;
 import com.kakaotech.team14backend.inner.point.repository.PointHistoryRepository;
 import com.kakaotech.team14backend.inner.point.repository.PointRepository;
-import com.kakaotech.team14backend.inner.post.model.Post;
 import com.kakaotech.team14backend.inner.post.repository.PostLikeCountRepository;
 import com.kakaotech.team14backend.inner.post.repository.PostRepository;
 import com.kakaotech.team14backend.outer.member.dto.GetMemberInfoResponseDTO;
 import com.kakaotech.team14backend.outer.member.dto.InstagramDetails;
 import com.kakaotech.team14backend.outer.member.dto.InstagramInfo;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -36,24 +31,16 @@ public class FindMemberInfoUsecase {
     Long totalPoint = findMemberTotalPoints(memberId);
     Long totalViewCount = postRepository.sumViewCountByMemberId(memberId);
 
-    List<Long> postIds = member.getPosts().stream().map(Post::getPostId)
-        .collect(Collectors.toList());
-
     boolean isLinked = !member.getInstaId().equals("none");
-
-    Long totalReceivedFireworks = pointHistoryRepository.receivedFireworksByReceivedId(memberId)
-        .map(list -> list.stream() // Optional<List<PointHistory>>를 Stream<PointHistory>로 변환
-            .map(PointHistory::getTransferPoint).filter(Objects::nonNull) // null 값 필터링
-            .mapToLong(Long::longValue).sum()).orElse(0L); // 리스트가 없거나 비어있을 경우 0을 반환
 
     if (isLinked) {
       return new GetMemberInfoResponseDTO(member.getMemberId(), member.getUserName(),
           member.getProfileImageUrl(), totalPoint, new InstagramInfo(true,
-          new InstagramDetails(totalLike, totalViewCount, totalReceivedFireworks)));
+          new InstagramDetails(totalLike, totalViewCount)));
     } else {
       return new GetMemberInfoResponseDTO(member.getMemberId(), member.getUserName(),
           member.getProfileImageUrl(), totalPoint,
-          new InstagramInfo(false, new InstagramDetails(null, null, null)));
+          new InstagramInfo(false, new InstagramDetails(null, null)));
     }
   }
 
--- src/main/java/com/kakaotech/team14backend/outer/member/dto/InstagramDetails.java
+++ src/main/java/com/kakaotech/team14backend/outer/member/dto/InstagramDetails.java
@@ -2,8 +2,7 @@ package com.kakaotech.team14backend.outer.member.dto;
 
 public record InstagramDetails(
     Long totalLikes,
-    Long totalViews,
-    Long totalReceivedfireworks
+    Long totalViews
 ) {
 
 }
