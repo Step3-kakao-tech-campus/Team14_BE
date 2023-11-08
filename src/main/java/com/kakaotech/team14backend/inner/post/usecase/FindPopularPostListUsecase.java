e26b26babb5d00ec6e1153a6fe8f1b3cc0c5de55
Sonny <hds650588@gmail.com>
2023-11-09 01:50:56 +0900

    refactor : LevelIndexes 클래스와 PostRandomFetcher 클래스 변경사항 반영

--- src/main/java/com/kakaotech/team14backend/inner/post/usecase/FindPopularPostListUsecase.java
+++ src/main/java/com/kakaotech/team14backend/inner/post/usecase/FindPopularPostListUsecase.java
@@ -1,12 +1,14 @@
 package com.kakaotech.team14backend.inner.post.usecase;
 
 import com.kakaotech.team14backend.common.RedisKey;
+import com.kakaotech.team14backend.inner.post.model.LevelIndexes;
 import com.kakaotech.team14backend.inner.post.model.Post;
 import com.kakaotech.team14backend.inner.post.model.PostRandomFetcher;
+import com.kakaotech.team14backend.inner.post.model.RandomIndexes;
 import com.kakaotech.team14backend.inner.post.repository.PostRepository;
 import com.kakaotech.team14backend.outer.post.dto.GetIncompletePopularPostDTO;
+import com.kakaotech.team14backend.outer.post.dto.GetPopularPostListRequestDTO;
 import com.kakaotech.team14backend.outer.post.dto.GetPopularPostListResponseDTO;
-import com.kakaotech.team14backend.inner.post.model.RandomIndexes;
 import com.kakaotech.team14backend.outer.post.mapper.PostMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -25,33 +27,34 @@ public class FindPopularPostListUsecase {
 
   private final RedisTemplate redisTemplate;
   private final PostRepository postRepository;
-  private final PostRandomFetcher postRandomFetcher;
-  public GetPopularPostListResponseDTO execute(Map<Integer, Integer> levelCounts, int size) {
+
+  public GetPopularPostListResponseDTO execute(GetPopularPostListRequestDTO getPopularPostListRequestDTO, int size) {
 
     List<GetIncompletePopularPostDTO> incompletePopularPostDTOS = new ArrayList<>();
     GetPopularPostListResponseDTO getPopularPostListResponseDTO;
 
-    Map<Integer, RandomIndexes> levelIndexes =  getLevelIndexes(levelCounts, size);
+    LevelIndexes levelIndexes = getLevelIndexes(getPopularPostListRequestDTO, size);
 
-    for (Map.Entry<Integer, RandomIndexes> entry : levelIndexes.entrySet()) {
+    for (Map.Entry<Integer, RandomIndexes> entry : levelIndexes.levelIndexes().entrySet()) {
       Integer level = entry.getKey();
       List<Integer> indexes = entry.getValue().getIndexes();
       for (Integer index : indexes) {
         Long postId = redisTemplate.opsForZSet().reverseRank(RedisKey.POPULAR_POST_KEY.getKey(), index);
         Optional<Post> optionalPost = postRepository.findById(postId);
-        if(optionalPost.isPresent()){
+        if (optionalPost.isPresent()) {
           Post post = optionalPost.get();
-          incompletePopularPostDTOS.add(new GetIncompletePopularPostDTO(post.getPostId(), post.getImage().getImageUri(), post.getHashtag(), post.getPostLikeCount().getLikeCount(), post.getPopularity(),level, post.getNickname()));
+          incompletePopularPostDTOS.add(new GetIncompletePopularPostDTO(post.getPostId(), post.getImage().getImageUri(), post.getHashtag(), post.getPostLikeCount().getLikeCount(), post.getPopularity(), level, post.getNickname()));
         }
       }
     }
-    getPopularPostListResponseDTO = PostMapper.from(incompletePopularPostDTOS, levelIndexes);
+    getPopularPostListResponseDTO = PostMapper.from(incompletePopularPostDTOS, levelIndexes.levelIndexes());
 
     return getPopularPostListResponseDTO;
   }
 
-  private Map<Integer, RandomIndexes> getLevelIndexes(Map<Integer, Integer> levelCounts, int limitSize) {
-    Map<Integer, RandomIndexes> levelIndexes = postRandomFetcher.fetchRandomIndexesForAllLevels(levelCounts, limitSize);
+  private LevelIndexes getLevelIndexes(GetPopularPostListRequestDTO getPopularPostListRequestDTO, int limitSize) {
+    PostRandomFetcher postRandomFetcher1 = new PostRandomFetcher(getPopularPostListRequestDTO.levelSize(), limitSize);
+    LevelIndexes levelIndexes = postRandomFetcher1.getLevelIndexes();
     return levelIndexes;
   }
 
