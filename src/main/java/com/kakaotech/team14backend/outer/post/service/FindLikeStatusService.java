5de56754aa1ce945dc26e32c86195701de4fe235
Beomsu <gobeumsu@gmail.com>
2023-11-09 14:15:56 +0900

    refactor : 좋아요 상태 관리 읽기 트랜잭션 설정

--- src/main/java/com/kakaotech/team14backend/outer/post/service/FindLikeStatusService.java
+++ src/main/java/com/kakaotech/team14backend/outer/post/service/FindLikeStatusService.java
@@ -4,9 +4,11 @@ import com.kakaotech.team14backend.inner.post.model.PostLike;
 import com.kakaotech.team14backend.inner.post.repository.PostLikeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class FindLikeStatusService {
 
   private final PostLikeRepository postLikeRepository;
