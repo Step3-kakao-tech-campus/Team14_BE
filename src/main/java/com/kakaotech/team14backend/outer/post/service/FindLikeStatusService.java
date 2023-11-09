11f260f4717f2e726454dd98ea6cbc0d1dd4e167
Beomsu <gobeumsu@gmail.com>
2023-11-09 14:12:08 +0900

    refactor : 최근 좋아요의 상태 점검 후 isLiked 반환

--- src/main/java/com/kakaotech/team14backend/outer/post/service/FindLikeStatusService.java
+++ src/main/java/com/kakaotech/team14backend/outer/post/service/FindLikeStatusService.java
@@ -2,10 +2,8 @@ package com.kakaotech.team14backend.outer.post.service;
 
 import com.kakaotech.team14backend.inner.post.model.PostLike;
 import com.kakaotech.team14backend.inner.post.repository.PostLikeRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
 
 @Component
 @RequiredArgsConstructor
@@ -14,13 +12,10 @@ public class FindLikeStatusService {
   private final PostLikeRepository postLikeRepository;
 
   public boolean execute(Long memberId, Long postId) {
-
-    List<PostLike> latestPostLike = postLikeRepository
-        .findFirstByMemberAndPostOrderByCreatedAtDesc(memberId, postId);
-    if (latestPostLike.isEmpty()) {
-      return false;
-    } else {
-      return latestPostLike.get(0).isLiked();
-    }
+    return postLikeRepository.findFirstByMemberAndPostOrderByCreatedAtDesc(memberId, postId)
+        .stream()
+        .findFirst()
+        .map(PostLike::isLiked)
+        .orElse(false);
   }
 }
