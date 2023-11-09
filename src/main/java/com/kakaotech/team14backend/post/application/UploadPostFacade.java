09d8814f3a33e6351bb0dbfdce4dcbe33c529c63
Sonny <hds650588@gmail.com>
2023-11-09 20:54:11 +0900

    feat : 게시물을 업로드할 때 필요한 관련 usecase들을 제어하는 UploadPostFacade 클래스 생성

---
+++ src/main/java/com/kakaotech/team14backend/post/application/UploadPostFacade.java
@@ -0,0 +1,33 @@
+package com.kakaotech.team14backend.post.application;
+
+import com.kakaotech.team14backend.image.application.CreateImage;
+import com.kakaotech.team14backend.image.domain.Image;
+import com.kakaotech.team14backend.point.appliation.GetPointUsecase;
+import com.kakaotech.team14backend.point.domain.GetPointPolicy;
+import com.kakaotech.team14backend.post.dto.CreatePostDTO;
+import com.kakaotech.team14backend.post.dto.UploadPostDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UploadPostFacade {
+
+  private final CreateImage createImage;
+  private final CreatePost createPostUsecase;
+  private final GetPointUsecase getPointUsecase;
+
+  @Transactional
+  public void uploadPost(UploadPostDTO uploadPostDTO) {
+    Image savedImage = createImage.execute(uploadPostDTO.uploadPostRequestDTO().getImage());
+    createPostUsecase.execute(makeCreatePostDTO(uploadPostDTO, savedImage));
+    getPointUsecase.execute(uploadPostDTO.member(), GetPointPolicy.GIVE_300_WHEN_UPLOAD);
+  }
+
+  private CreatePostDTO makeCreatePostDTO(UploadPostDTO uploadPostDTO, Image savedImage) {
+    return new CreatePostDTO(savedImage, uploadPostDTO.uploadPostRequestDTO(),
+        uploadPostDTO.member());
+  }
+}
