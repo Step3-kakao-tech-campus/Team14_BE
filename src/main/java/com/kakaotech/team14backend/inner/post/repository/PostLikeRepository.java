18245bba1b59aa67a80ec806d9b2b1f082980695
Beomsu <gobeumsu@gmail.com>
2023-11-09 13:48:43 +0900

    refactor : native query to jpa query

--- src/main/java/com/kakaotech/team14backend/inner/post/repository/PostLikeRepository.java
+++ src/main/java/com/kakaotech/team14backend/inner/post/repository/PostLikeRepository.java
@@ -1,7 +1,5 @@
 package com.kakaotech.team14backend.inner.post.repository;
 
-import com.kakaotech.team14backend.inner.member.model.Member;
-import com.kakaotech.team14backend.inner.post.model.Post;
 import com.kakaotech.team14backend.inner.post.model.PostLike;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,9 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
 
 
-  @Query(
-      value = "SELECT * FROM post_like pl WHERE pl.member_id = :memberId AND pl.post_id = :postId ORDER BY pl.created_at DESC LIMIT 1",
-      nativeQuery = true
-  )
+  @Query("select pl from post_like pl where pl.member.memberId = ?1 and pl.post.postId = ?2 order by pl.createdAt desc")
   Optional<PostLike> findFirstByMemberAndPostOrderByCreatedAtDesc(Long memberId, Long postId);
 }
