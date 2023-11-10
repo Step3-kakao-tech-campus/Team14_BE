b0b8313e42795bf186fe3fd16b736a517e3fc593
Beomsu <gobeumsu@gmail.com>
2023-11-10 18:55:04 +0900

    refactor : FindMyPost 최적화

--- src/main/java/com/kakaotech/team14backend/post/application/FindMyPostUsecase.java
+++ src/main/java/com/kakaotech/team14backend/post/application/FindMyPostUsecase.java
@@ -3,6 +3,7 @@ package com.kakaotech.team14backend.post.application;
 import com.kakaotech.team14backend.post.application.command.FindLikeStatusCommand;
 import com.kakaotech.team14backend.post.domain.Post;
 import com.kakaotech.team14backend.post.dto.GetMyPostResponseDTO;
+import com.kakaotech.team14backend.post.dto.GetPostDTO;
 import com.kakaotech.team14backend.post.infrastructure.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -16,10 +17,12 @@ public class FindMyPostUsecase {
   private final PostRepository postRepository;
   private final FindLikeStatusCommand findLikeStatusCommand;
 
-  public GetMyPostResponseDTO execute(Long memberId, Long postId) {
+  public GetMyPostResponseDTO execute(GetPostDTO getPostDTO) {
+    Long memberId = getPostDTO.memberId();
+    Long postId = getPostDTO.postId();
+
     Post post = postRepository.findByPostIdAndMemberId(memberId, postId);
     boolean isLiked = findLikeStatusCommand.execute(memberId, postId);
-    GetMyPostResponseDTO getPostResponseDTO = PostMapper.from(post, isLiked, memberId);
-    return getPostResponseDTO;
+    return PostMapper.from(post, isLiked, memberId);
   }
 }
