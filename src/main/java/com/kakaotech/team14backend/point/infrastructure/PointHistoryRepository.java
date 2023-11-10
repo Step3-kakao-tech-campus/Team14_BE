8f8deece413898c0cb4ceb0b9c0b73d88bde0970
Sonny <hds650588@gmail.com>
2023-11-10 16:33:49 +0900

    refactor : 해당 클래스들 point/infrastructure 폴더 밑으로 이동

--- src/main/java/com/kakaotech/team14backend/point/infrastructure/PointHistoryRepository.java
+++ src/main/java/com/kakaotech/team14backend/point/infrastructure/PointHistoryRepository.java
@@ -1,10 +1,11 @@
-package com.kakaotech.team14backend.inner.point.repository;
+package com.kakaotech.team14backend.point.infrastructure;
 
-import com.kakaotech.team14backend.inner.point.model.PointHistory;
-import java.util.List;
-import java.util.Optional;
+import com.kakaotech.team14backend.point.domain.PointHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
 
